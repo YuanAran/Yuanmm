4073563f232f75d1593972d0766bbe28630b3371
YuanAran <19185402557@163.com>
2024-11-20 22:25:32 +0800

    修改

--- work.../school_lib/src/main/java/MergeSort.java
+++ work.../school_lib/src/main/java/MergeSort.java
@@ -48,11 +48,4 @@ public class MergeSort {
             index++;
         }
     }
-    public static void main(String[] args) {
-        int[] a={32,56,12,68,55,49,14};
-        mergeSort(a,0,a.length-1);
-        for(int i:a){
-            System.out.println(i);
-        }
-    }
 }
--- work.../school_lib/src/main/java/Test.java
+++ work.../school_lib/src/main/java/Test.java
@@ -1,2 +1,9 @@
 public class Test {
+    public static void main(String[] args) {
+        int[] a={32,56,12,68,55,49,14};
+        MergeSort.mergeSort(a,0,a.length-1);
+        for (int i:a){
+            System.out.println(i);
+        }
+    }
 }
