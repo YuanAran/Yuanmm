36db9b19f6b46f97fac76ffdd96936eedd7d7709
YuanAran <19185402557@163.com>
2025-03-17 17:35:20 +0800

    Delete work.../school_lib/src/main/java directory

--- work.../school_lib/src/main/java/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        int[] a={32,56,12,68,55,49,14};
-        MergeSort.mergeSort(a,0,a.length-1);
-        for (int i:a){
-            System.out.println(i);
-        }
-    }
-}
